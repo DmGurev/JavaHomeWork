929614ecc03c74f9eed450236a3f3fb5d4449d71
DmGurev <ha121080gdm@gmail.com>
2021-10-22 11:57:34 +0300

    init commit

---
+++ src/com/pb/gurev/hw3/bingo.java
@@ -0,0 +1,90 @@
+package com.pb.gurev.hw3;
+import java.util.Random;
+import java.util.Scanner;
+public class bingo {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        Random rand = new Random();
+        int numberToGuess = rand.nextInt(100);
+        int numberOfTries = 0;
+        int guess;
+        boolean x = false;
+        System.out.println("Отгадайте число от 0 до 100");
+        while (x == false) {
+            System.out.println("Итак... Ваше число!");
+            guess = input.nextInt();
+            numberOfTries++;
+            if (guess == numberToGuess) {
+               x = true;
+                System.out.println("Вы выиграли! ");
+                System.out.println("Правильный ответ: " + numberToGuess);
+                System.out.println("Количество попыток: " + numberOfTries);
+            }
+             else if  (guess < numberToGuess) {
+                System.out.println("Мало");
+            }
+            else if (guess > numberToGuess) {
+                System.out.println("Много");
+            }
+
+            continue;
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        //  String playAgain;
+        //do {
+           // int answer = MIN_VALUE + generator.nextInt(MAX_VALUE);
+         //   System.out.println("Отгадайте число от 0 до 100");
+          //  boolean correct =
+
+
+        //int numberToGuess = 0 + generator.nextInt (100);
+        //int numberOfTries = 0 + generator.nextInt (10000000);
+        //int x;
+        //x>0||<100;
+   //while (x!= numberToGuess) {
+       //System.out.println("Отгадайте число от 0 до 100");
+      // x = scan.nextInt();
+      //if (x < numberToGuess) {
+         //  System.out.println("Слишком маленькое число, выберите еще");
+      // } else if (x > numberToGuess)
+           //System.out.println("Слишком большое число, выберите еще");
+           //else if (guess > 100 || < 1 )
+           //System.out.println ("Соблюдайте предложенный диапазон! От 1 до 100");
+       //else if (x == numberToGuess)
+          // System.out.println("Ура! Вы угадали!");
+       //System.exit(0);
+       //} else ()
+      // System.out.println("Вы использовали все 10 попыток! Фарта и Любви!");
+      // System.out.println("Правильный ответ " + numberToGuess);
+     //  break;
+       //if (numberOfTries > 10);{
+       // System.out.println ("Вы использовали все 10 попыток! Фарта и Любви!");
+       // System.out.println ("Правильный ответ " + numberToGuess);
+
+
+   //} while (numberOfTries < 11);
+
+//}
+
+
+
+
+
+
