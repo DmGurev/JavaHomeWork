213b79a32aeb1c8b00929123c6461391651f7016
DmGurev <ha121080gdm@gmail.com>
2021-11-25 22:41:46 +0200

    init commit

--- src/com/pb/gurev/hw8/Auth.java
+++ src/com/pb/gurev/hw8/Auth.java
@@ -5,49 +5,69 @@ import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class Auth{
-    private String login;
-    private String password;
 
-    public void SignUp(){
+public class Auth {
 
-/////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-        Pattern pattern = Pattern.compile("[A-Za-z0-9]{5,20}");
-        Matcher matcher = pattern.matcher(login);
-        Scanner scan = new Scanner(System.in);
-        login = scan.nextLine();
-        System.out.println("Введите логин. Требования: 5-20 символов (латинских букв и цифр)");
+    private String login = "login ";
+    private String password = "password";
 
-        if (Pattern.matches("[A-Za-z0-9]{5,20}", login)) {
-            System.out.println("Вы ввели логин с учетом требований. Ваш логин:   " + login);
-            this.login = login;
-        }else if (!Pattern.matches("[A-Za-z0-9]{5,20}", login)) ;
-        throw new  WrongLoginException;
-       // WrongLoginException ex1 = new WrongLoginException();
-        ////////////////////////////////////////////////////////////////////////////////////////////////////
-
-            String login2 = scan.nextLine();
-            Pattern pattern2 = Pattern.compile("[A-Za-z_]{5,}");
-            Matcher matcher2 = pattern.matcher(password);
-            //Scanner scan = new Scanner(System.in);
-            password = scan.nextLine();
-            System.out.println("Введите пароль. Требования: не менее 5 символов (латинские буквы и цифры, нижнее подчеркивание)");
-
-            if (Pattern.matches("[A-Za-z0-9_]{5,}", password)) {
-                System.out.println("Вы ввели пароль с учетом требований. Ваш логин:   " + password);
-                this.password = password;
-            } else if (!Pattern.matches("[A-Za-z0-9_]{5,}", password));
-            throw new WrongPasswordException(password);
-            WrongPasswordException ex2 = new WrongPasswordException();
+    public void signUp(String login, String password, String confirmPassword) throws WrongLoginException, WrongPasswordException {
+        if (login == null || !login.matches("[a-zA-Z0-9]{5,20}")) {
+            throw new WrongLoginException("Login length must be between 5 and 20 symbols, contains only letters and numbers");
+        }
+        if (password == null || !password.matches("[a-zA-Z0-9_]{5,}")) {
+            throw new WrongPasswordException("Password length must be >= 5 symbols, contains only letters, numbers and symbol \"_\"");
+        }
+        if (!password.equals(confirmPassword)) {
+            throw new WrongPasswordException("Password not confirmed");
+        }
+        this.login = login;
+        this.password = password;
     }
-    public static void SignIn (String login, String password) throws WrongLoginException {
-        if (!login.equals(login)&&!password.equals(login)){
-            throw new  WrongLoginException(login);}
-        else {
-            System.out.println("Добро пожаловать!");
+    public void signIn(String login, String password) throws WrongLoginException, WrongPasswordException {
+//        if (!this.login.equals(login)|| !this.password.equals(password)){
+//            throw new WrongLoginException("User not found");
+//        }
+        if (!this.login.equals(login)) {
+            throw new WrongLoginException("User not found");
+        }
+        if (!this.password.equals(password)) {
+            throw new WrongLoginException("User not found");
         }
     }
 }
+
+
+//        if (Pattern.matches("[A-Za-z0-9]{5,20}", login)) {
+//            System.out.println("Вы ввели логин с учетом требований. Ваш логин:   " + login);
+//            this.login = login;
+//        }else if (!Pattern.matches("[A-Za-z0-9]{5,20}", login))  {
+//        throw new  WrongLoginException ("Не выполнены требования к логину (5-20 латинских символов - строчных и заглавных букв и цифр"); }
+//       // WrongLoginException ex1 = new WrongLoginException();
+//        ////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//            String login2 = scan.nextLine();
+//            Pattern pattern2 = Pattern.compile("[A-Za-z_]{5,}");
+//            Matcher matcher2 = pattern.matcher(password);
+//            //Scanner scan = new Scanner(System.in);
+//            password = scan.nextLine();
+//            System.out.println("Введите пароль. Требования: не менее 5 символов (латинские буквы и цифры, нижнее подчеркивание)");
+//
+//            if (Pattern.matches("[A-Za-z0-9_]{5,}", password)) {
+//                System.out.println("Вы ввели пароль с учетом требований. Ваш логин:   " + password)}
+//                this.password = password;
+//            } else if (!Pattern.matches("[A-Za-z0-9_]{5,}", password));
+//            throw new WrongPasswordException(password);
+//            WrongPasswordException ex2 = new WrongPasswordException();
+//    }
+//    public static void SignIn (String login, String password) throws WrongLoginException {
+//        if (!login.equals(login)&&!password.equals(login)){
+//            throw new  WrongLoginException(login);}
+//        else {
+//            System.out.println("Добро пожаловать!");
+//        }
+//    }
+//}
 
 
 
--- src/com/pb/gurev/hw8/OnlineShop.java
+++ src/com/pb/gurev/hw8/OnlineShop.java
@@ -1,17 +1,70 @@
 package com.pb.gurev.hw8;
 
+import java.util.Scanner;
+
 public class OnlineShop {
-    public static void main(String[] args) throws Exception
-    Auth auth = new Auth();
-    try {
-        auth.signUp();
-    catch (WrongLoginException ex1)  {
-            ex1.printStackTrace();
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        Auth auth = new Auth();
+
+        while (true) {
+            System.out.println();
+            System.out.println("Hello in OnlineShop");
+            System.out.println("1. Sign up as new user");
+            System.out.println("2. Sign in");
+            System.out.println("3. Exit");
+
+            String option = scan.nextLine();
+
+            switch (option) {
+                case "1":
+                    signUp(scan, auth);
+                    break;
+                case "2":
+                    signIn(scan, auth);
+                    break;
+                case "3":
+                    System.out.println("Bye!");
+                    return;
+                default:
+                    System.out.println("Wrong menu number!");
+            }
         }
-        auth.SignIn();{
-        catch (WrongPasswordException ex2) }
-        ex1.printStackTrace();}
+    }
 
+    private static void signUp(Scanner scan, Auth auth) {
+        System.out.println("Input login: ");
+        String login = scan.nextLine();
 
+        System.out.println("Input password: ");
+        String password = scan.nextLine();
+
+        System.out.println("Confirm password: ");
+        String confirmPassword = scan.nextLine();
+
+        try {
+            auth.signUp(login, password, confirmPassword);
+            System.out.println("New user registered successful!");
+        } catch (WrongLoginException ex) {
+            System.out.println("Login error: " + ex.getMessage());
+        } catch (WrongPasswordException ex) {
+            System.out.println("Password error: " + ex.getMessage());
+        }
+    }
+
+    private static void signIn(Scanner scan, Auth auth) {
+        System.out.println("Input login: ");
+        String login = scan.nextLine();
+
+        System.out.println("Input password: ");
+        String password = scan.nextLine();
+
+        try {
+            auth.signIn(login, password);
+            System.out.println("Hello " + login + ", sing in successful!");
+        } catch (WrongLoginException | WrongPasswordException ex) {
+            System.out.println("Sig in error: " + ex.getMessage());
+        }
+    }
 }
-
